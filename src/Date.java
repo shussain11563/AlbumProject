b968db9468a64f95d7a1d99ff932610c69b911cf
David Lam <dlamh19@gmail.com>
2021-09-27 15:19:00 -0400

    Added some javadoc comments and fixed some spacing

--- src/Date.java
+++ src/Date.java
@@ -50,7 +50,7 @@ public class Date implements Comparable<Date>
     public boolean isValid() {
         Calendar currentDate = Calendar.getInstance();
 
-        if(this.year < THE_EIGHTYS && this.year ) {
+        if(this.year < THE_EIGHTYS || this.year > currentDate.get(Calendar.YEAR)) {
             return false;
         }
         else if(this.month >= MONTH_MIN && this.month <= MONTH_MAX) {
@@ -140,6 +140,7 @@ public class Date implements Comparable<Date>
      */
     public static void main(String[] args) {
         // test case #1, a date with a year before 1980 should be invalid
+        // fail
         Date date = new Date("1/2/1900");
         System.out.println("Test Case #1");
         if(date.isValid())
@@ -148,6 +149,7 @@ public class Date implements Comparable<Date>
             System.out.println("Fail.");
 
         // test case #2, a date with an invalid month
+        // fail
         date = new Date("15/2/2000");
         System.out.println("Test Case #2");
         if(date.isValid())
@@ -156,6 +158,7 @@ public class Date implements Comparable<Date>
             System.out.println("Fail.");
 
         // test case #3, a date with an invalid day
+        // fail
         date = new Date("5/50/2000");
         System.out.println("Test Case #3");
         if(date.isValid())
@@ -164,6 +167,7 @@ public class Date implements Comparable<Date>
             System.out.println("Fail.");
 
         // test case #4, check for valid leap year
+        // pass
         date = new Date("2/29/2016");
         System.out.println("Test Case #4");
         if(date.isValid())
@@ -172,6 +176,7 @@ public class Date implements Comparable<Date>
             System.out.println("Fail.");
 
         // test case #5, check for invalid day for leap year
+        // fail
         date = new Date("2/30/2016");
         System.out.println("Test Case #5");
         if(date.isValid())
@@ -180,6 +185,7 @@ public class Date implements Comparable<Date>
             System.out.println("Fail.");
 
         // test case #6, check for invalid day on not a leap year
+        // fail
         date = new Date("2/29/2013");
         System.out.println("Test Case #6");
         if(date.isValid())
@@ -188,6 +194,7 @@ public class Date implements Comparable<Date>
             System.out.println("Fail.");
 
         // test case #7, check the max/min for day/month/year
+        // fail
         date = new Date("30/27123/2000");
         System.out.println("Test Case #7");
         if(date.isValid())
@@ -196,6 +203,7 @@ public class Date implements Comparable<Date>
             System.out.println("Fail.");
 
         // test case #8, check if the day is valid
+        // fail
         date = new Date("301/5/2005");
         System.out.println("Test Case #8");
         if(date.isValid())
@@ -204,6 +212,7 @@ public class Date implements Comparable<Date>
             System.out.println("Fail.");
 
         // test case #9, check if the month is valid
+        // fail
         date = new Date("1/50/2021");
         System.out.println("Test Case #9");
         if(date.isValid())
@@ -212,12 +221,12 @@ public class Date implements Comparable<Date>
             System.out.println("Fail.");
 
         // test case #10, check if the month is between 1980 and current year
+        // fail
         date = new Date("9/1/2070");
         System.out.println("Test Case #10");
         if(date.isValid())
             System.out.println("Pass.");
         else
             System.out.println("Fail.");
-
     }
 }
